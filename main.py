7ac2ca609715d955bdb0d1b3f4daac2c762b190a
Felippe <frafaelrls@gmail.com>
2022-01-29 16:07:12 -0300

    updating main

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from logica import devolve_significado
 def user():
     entrada = ""
     while entrada != "Sair":
-        entrada = input(f"Olá,\nInsira a palavra desejada ou digite 'Sair'\n")
+        entrada = input(f"\nOlá,\nInsira a palavra desejada."
+                        f"\nDigite 'Sair' para encerrar o programa."
+                        f"\nDigite '?' para acessar a lista de palavras"
+                        f" salvas.\n")
         devolve_significado(entrada)
 
 
