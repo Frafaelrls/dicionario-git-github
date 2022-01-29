a5abe5a1c288e802bd47d80fd8e6db564ed34e50
Nádia Oliveira <nadiaaoliverr@gmail.com>
2022-01-29 13:51:22 -0300

    Adding information to the dictionary

--- dicionario.py
+++ dicionario.py
@@ -1,2 +1,2 @@
-dicionario = {'teste': 'Significado um', 'teste 2': 'Significado doi'}
-print(dicionario['teste'])
+dicionario = {'git push': 'Envia as alterações locais para o repositório remoto', 'teste 2': 'Significado doi'}
+print(dicionario['git push'])
