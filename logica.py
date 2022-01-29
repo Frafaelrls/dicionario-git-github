d2ad0a6cb176e5910eec166e8e9a971e1c8b1727
Felippe <frafaelrls@gmail.com>
2022-01-29 16:06:56 -0300

    updating logica.py

--- logica.py
+++ logica.py
@@ -5,5 +5,10 @@ def devolve_significado(entrada):
     banco_dados = dic()
     if entrada in banco_dados:
         print(banco_dados[entrada])
+
+    if entrada == "?":
+        lista = banco_dados.keys()
+        for nomes in lista:
+            print(nomes)
     else:
         print("Palavra não encontrada")
