1c7fa3e54ffbefa96a9485e1d5b092d2cf86d3b7
Felippe <frafaelrls@gmail.com>
2022-01-29 14:43:10 -0300

    updating dicionario.py

--- dicionario.py
+++ dicionario.py
@@ -1,10 +1,12 @@
-dicionario = {'git push': 'Envia as alterações locais para o repositório remoto', 'teste 2': 'Significado doi'}
-print(dicionario['git push'])
-print("Teste")
-def dic(user_input):
-    dicionario = {'teste': 'Significado um', 'teste 2': 'Significado dois'}
-    print(dicionario[user_input])
+# criar um arquivo para lógica.py
+def dic(palavra):
+    if user_input in dicionario:
+        print(dicionario[user_input])
+    else:
+        print(f"Palavra não encontrada!")
 
-
+# Manter no arquivo dicionario.py
+dicionario = {'git push': 'Envia as alterações locais para do repositório local para o remoto', 'teste 2': 'Significado dois'}
+# Enviar para arquivo main.py
 user_input = input(f"Olá,\nInsira a palavra desejada\n")
 dic(user_input)
